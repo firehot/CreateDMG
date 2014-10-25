b837686e38f89fa436951be8ea48f845c417d56a
wzqcongcong <wzqcongcong@sina.com>
2014-10-25 11:56:40 +0800

    custom

--- support/dmg-license.py
+++ support/dmg-license.py
@@ -45,18 +45,13 @@ def mktemp(dir=None, suffix=''):
 
 
 def main(options, args):
-    dmgFile, license = args
+    dmgFile, license_en, license_ja = args
     with mktemp('.') as tmpFile:
         with open(tmpFile, 'w') as f:
             f.write("""data 'LPic' (5000) {
-    $"0002 0011 0003 0001 0000 0000 0002 0000"
-    $"0000 000E 0006 0001 0005 0007 0000 0007"
-    $"0008 0000 0047 0009 0000 0034 000A 0001"
-    $"0035 000B 0001 0020 000C 0000 0011 000D"
-    $"0000 005B 0004 0000 0033 000F 0001 000C"
-    $"0010 0000 000B 000E 0000"
+    $"0000 0002 0000 0000 0000 000E 0006 0001"
 };\n\n""")
-            with open(license, 'r') as l:
+            with open(license_en, 'r') as l:
                 f.write('data \'TEXT\' (5002, "English") {\n')
                 for line in l:
                     if len(line) < 1000:
@@ -68,24 +63,51 @@ def main(options, args):
                                     liner.strip().replace('"', '\\"') +
                                     '. \\n"\n')
                 f.write('};\n\n')
-            f.write("""resource 'STR#' (5002, "English") {
-    {
-        "English",
-        "Agree",
-        "Disagree",
-        "Print",
-        "Save...",
-        "IMPORTANT - By clicking on the \\"Agree\\" button, you agree "
-        "to be bound by the terms of the License Agreement.",
-        "Software License Agreement",
-        "This text cannot be saved. This disk may be full or locked, or the "
-        "file may be locked.",
-        "Unable to print. Make sure you have selected a printer."
-    }
-};""")
+            f.write("""data 'STR#' (5002, "English") {
+    $"0006 0745 6E67 6C69 7368 0541 6772 6565"
+    $"0844 6973 6167 7265 6505 5072 696E 7407"
+    $"5361 7665 2E2E 2E7B 4966 2079 6F75 2061"
+    $"6772 6565 2077 6974 6820 7468 6520 7465"
+    $"726D 7320 6F66 2074 6869 7320 6C69 6365"
+    $"6E73 652C 2070 7265 7373 2022 4167 7265"
+    $"6522 2074 6F20 696E 7374 616C 6C20 7468"
+    $"6520 736F 6674 7761 7265 2E20 2049 6620"
+    $"796F 7520 646F 206E 6F74 2061 6772 6565"
+    $"2C20 7072 6573 7320 2244 6973 6167 7265"
+    $"6522 2E"
+};\n\n""")
+            with open(license_ja, 'r') as l:
+                f.write('data \'TEXT\' (5006, "Japanese") {\n')
+                for line in l:
+                    if len(line) < 1000:
+                        f.write('    "' + line.strip().replace('"', '\\"').decode("SHIFT_JIS").encode("SHIFT_JIS") +
+                                '\\n"\n')
+                    else:
+                        for liner in line.split('.'):
+                            f.write('    "' +
+                                    liner.strip().replace('"', '\\"').decode("SHIFT_JIS").encode("SHIFT_JIS") +
+                                    '. \\n"\n')
+                f.write('};\n\n')
+            f.write("""data 'STR#' (5006, "Japanese") {
+    $"0006 084A 6170 616E 6573 650A 93AF 88D3"
+    $"82B5 82DC 82B7 0C93 AF88 D382 B582 DC82"
+    $"B982 F108 88F3 8DFC 82B7 82E9 0795 DB91"
+    $"B62E 2E2E B496 7B83 5C83 7483 6783 4583"
+    $"4783 418E 6797 708B 9691 F88C 5F96 F182"
+    $"CC8F F08C 8F82 C993 AF88 D382 B382 EA82"
+    $"E98F EA8D 8782 C982 CD81 4183 5C83 7483"
+    $"6783 4583 4783 4182 F083 4383 9383 5883"
+    $"6781 5B83 8B82 B782 E982 BD82 DF82 C981"
+    $"7593 AF88 D382 B582 DC82 B781 7682 F089"
+    $"9F82 B582 C482 AD82 BE82 B382 A281 4281"
+    $"4093 AF88 D382 B382 EA82 C882 A28F EA8D"
+    $"8782 C982 CD81 4181 7593 AF88 D382 B582"
+    $"DC82 B982 F181 7682 F089 9F82 B582 C482"
+    $"AD82 BE82 B382 A281 42"
+};\n\n""")
         os.system('/usr/bin/hdiutil unflatten -quiet "%s"' % dmgFile)
-        os.system('%s "%s/"*.r %s -a -o "%s"' %
-                  (options.rez, options.flat_carbon, tmpFile, dmgFile))
+        os.system('%s -a %s -o "%s"' %
+                  (options.rez, tmpFile, dmgFile))
 
         os.system('/usr/bin/hdiutil flatten -quiet "%s"' % dmgFile)
         if options.compression is not None:
@@ -115,15 +137,6 @@ if __name__ == '__main__':
         help='The path to the Rez tool. Defaults to %default'
     )
     parser.add_option(
-        '--flat-carbon',
-        '-f',
-        action='store',
-        default='/Applications/Xcode.app/Contents/Developer/Platforms'
-                '/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk'
-                '/Developer/Headers/FlatCarbon',
-        help='The path to the FlatCarbon headers. Defaults to %default'
-    )
-    parser.add_option(
         '--compression',
         '-c',
         action='store',
@@ -133,8 +146,7 @@ if __name__ == '__main__':
              'Choices are bz2 and gz.'
     )
     options, args = parser.parse_args()
-    cond = len(args) != 2 or not os.path.exists(options.rez) \
-        or not os.path.exists(options.flat_carbon)
+    cond = not os.path.exists(options.rez)
     if cond:
         parser.print_usage()
         sys.exit(1)
